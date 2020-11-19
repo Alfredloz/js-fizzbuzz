eb243737f2822d5b354304afd4e7eebb84bfa19c
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-19 18:15:26 +0100

    esercizio consegnato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,17 @@
 //programma che dia numeri da 1 a 100
-for (var i = 0; i < 100; i++) {
-  var numeri = i + 1;
-  var listaNumeri = [numeri];
-  if (i % 3 == 0) {
-    numeri == 'ciao';
+for (var i = 1; i < 101; i++) {
+  if (i % 3 == 0 && i % 5== 0) {
+    console.log('fizzbuzz');
+  } else if (i % 3 == 0) {
+    console.log('fizz');
+  } else if (i % 5 == 0) {
+    console.log('buzz');
+  } else {
     console.log(i);
   }
 }
 
-//ciclo while
-// var i = 0;
-// while (i < 100) {
-//     var numero = i +1;
-//     console.log(numero);
-//     if (i % 3 != 0) {
-//       console.log(i='ciao');
-//     }
-//     i++;
-// }
 
-// multipli di 3: stampa 'Fizz' al posto del numero
+
 //multipli di 5: stampa 'Buzz' al possto del numero
 // per entrambi i multipli : parola 'FizzBuzz';
